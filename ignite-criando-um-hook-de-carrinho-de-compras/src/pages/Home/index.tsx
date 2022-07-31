262b0b2e1cd897e9dc6cf428489dec5191fe101a
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-07-31 09:19:38 -0300

    commit

--- ignite-criando-um-hook-de-carrinho-de-compras/src/pages/Home/index.tsx
+++ ignite-criando-um-hook-de-carrinho-de-compras/src/pages/Home/index.tsx
@@ -22,58 +22,44 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  const [products, setProducts] = useState<ProductFormatted[]>([]);
-  const { addProduct, cart } = useCart();
+  // const [products, setProducts] = useState<ProductFormatted[]>([]);
+  // const { addProduct, cart } = useCart();
 
-  const cartItemsAmount = cart.reduce((sumAmount, product) => {
-    const newSumAmount = { ...sumAmount };
-    newSumAmount[product.id] = product.amount;
-
-    return newSumAmount;
-
-  }, {} as CartItemsAmount)
+  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
+  //   // TODO
+  // }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      const response = await api.get<Product[]>(`products`);
-
-      const data = response.data.map(product => ({
-        ...product,
-        priceFormatted: formatPrice(product.price)
-      }))
-
-      setProducts(data);
+      // TODO
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    addProduct(id);
+    // TODO
   }
 
   return (
     <ProductList>
-      {products.map(product => (
-        <li key={product.id}>
-          <img src={product.image} alt={product.title} />
-          <strong>{product.title}</strong>
-          <span>{product.priceFormatted}</span>
-          <button
-            type="button"
-            data-testid="add-product-button"
-            onClick={() => handleAddProduct(product.id)}
-          >
-            <div data-testid="cart-product-quantity">
-              <MdAddShoppingCart size={16} color="#FFF" />
-              {cartItemsAmount[product.id] || 0} 
-            </div>
+      <li>
+        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
+        <strong>Tênis de Caminhada Leve Confortável</strong>
+        <span>R$ 179,90</span>
+        <button
+          type="button"
+          data-testid="add-product-button"
+        // onClick={() => handleAddProduct(product.id)}
+        >
+          <div data-testid="cart-product-quantity">
+            <MdAddShoppingCart size={16} color="#FFF" />
+            {/* {cartItemsAmount[product.id] || 0} */} 2
+          </div>
 
-            <span>ADICIONAR AO CARRINHO</span>
-          </button>
-        </li>
-
-      ))}
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
     </ProductList>
   );
 };
